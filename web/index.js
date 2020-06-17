f40ba49f7c6eb4722e158edcc99b94a08e44fc52
Roberto Torres <jr.torres.roberto@gmail.com>
2020-06-16 23:30:48 -0500

    hover highlighting and clicking to start and pause

--- web/index.js
+++ web/index.js
@@ -1,25 +1,43 @@
-const _maxSeconds = 60;
+const _maxSeconds = 5;
 let time = 0;
-
+let timer;
+let isPaused = false;
 
 function startTimer() {
-    if (time == _maxSeconds) {
+    if (time === _maxSeconds) {
         time = 0;
+        isPaused = false;
     }
 
-    if (time > 0) {
+    if (time > 0 && !isPaused) {
+        clearInterval(timer);
+        isPaused = true;
         return;
     }
 
-    const timer = setInterval(getTime, 1000);
+    timer = setInterval(getTime, 1000);
 
     setTimeout(() => {
         clearInterval(timer);
     }, 1000 * _maxSeconds)
+
+    isPaused = false;
 }
 
 function getTime() {
     time += 1;
-    
-    document.getElementById("timer").innerHTML = time;
+    var timeStr = '';
+
+    if (time < 10) {
+        timeStr = '0' + time;
+    } else {
+        timeStr = time;
+    }
+    const currentTime = document.getElementById("timer").innerHTML; 
+
+    if (_maxSeconds <= currentTime && time >= _maxSeconds) {
+        return;
+    }
+
+    document.getElementById("timer").innerHTML = timeStr;
 }
